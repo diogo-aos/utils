b9568b8d6ef6f55dbf9810ce15d065360619d646
Diogo Silva <dasilva@academiafa.edu.pt>
2016-12-15 16:25:53 +0000

    more i3 keybindings; i3 python scripts

---
+++ i3/cycle-workspace.py
@@ -0,0 +1,21 @@
+import i3
+
+
+def move_workspace(workspace, output):
+    #  i3.command('workspace', workspace['name'])
+    i3.command('move', 'workspace to output ' + output['name'])
+    i3.command('workspace', workspace['name'])
+
+
+outputs = i3.get_outputs()
+workspaces = i3.get_workspaces()
+
+f_ws = [ws for ws in workspaces if ws['focused']][0]  # focused workspace
+f_output = [o for o in outputs if o['name'] == f_ws['output']][0]  # focused output
+a_outputs = [o for o in outputs if o['active']]  # active outputs
+
+# if there is only 1 output available, nothing to do
+if len(a_outputs) != 1:
+    idx = a_outputs.index(f_output)
+    next_output = a_outputs[(idx + 1) % len(a_outputs)]
+    move_workspace(f_ws, next_output)
